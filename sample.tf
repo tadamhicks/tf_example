8ff460c6126ff04586cba5093701935c2366a550
Adam Hicks <thomas.adam.hicks@gmail.com>
2020-05-26 12:47:11 -0600

    interpolation

--- sample.tf
+++ sample.tf
@@ -12,16 +12,16 @@ variable "secret_key" {}
 #################################
 provider "aws" {
 	region = "us-east-1"
-	access_key = "${var.access_key}"
-	secret_key = "${var.secret_key}"
+	access_key = "var.access_key"
+	secret_key = "var.secret_key"
 }
 
-resource "aws_instance" "example" {
+resource "aws_instance" "hero" {
   ami           = "ami-0edfed9d6e9a4031b"
   instance_type = "t2.micro"
 }
 
-resource "aws_instance" "second_example" {
+resource "aws_instance" "heorine" {
   ami           = "ami-0edfed9d6e9a4031b"
   instance_type = "t2.micro"
 }
