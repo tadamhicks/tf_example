28b3246e1585f4198aa67612927daa2a4f290c75
Adam Hicks <thomas.adam.hicks@gmail.com>
2020-05-26 13:37:51 -0600

    best

--- sample.tf
+++ sample.tf
@@ -1,25 +1,25 @@
 #################################
 ##			Variables		   ##
 #################################
-# variable "access_key" {}
-# variable "secret_key" {}
-# variable "ami_id" {}
-# variable "amazon_region" {}
+variable "access_key" {}
+variable "secret_key" {}
 
 
 #################################
 ##			Provider		   ##
 #################################
 provider "aws" {
-	region = "us-east-1"
+	region = "us-west-1"
+	access_key = "${var.access_key}"
+	secret_key = "${var.secret_key}"
 }
 
 resource "aws_instance" "superman" {
-  ami           = "ami-0edfed9d6e9a4031b"
+  ami           = "ami-0b91a410940e82c54"
   instance_type = "t2.micro"
 }
 
 resource "aws_instance" "batman" {
-  ami           = "ami-0edfed9d6e9a4031b"
+  ami           = "ami-0b91a410940e82c54"
   instance_type = "t2.micro"
 }
